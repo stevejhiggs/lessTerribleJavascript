862faa624bab235acead863905651af962e93fff
Steve Higgs <shiggs@opentable.com>
2015-10-08 11:57:17 +0100

    add linting - currently fails

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var webpack = require('webpack');
 var gutil = require('gulp-util');
+var eslint = require('gulp-eslint');
 
 gulp.task('bundle:dev', function (done) {
     webpack(require('./webpack.config'), function (err, stats) {
@@ -14,8 +15,7 @@ gulp.task('bundle:dev', function (done) {
     });
 });
 
-gulp.task('default', ['bundle:dev'], function () {
-});
+
 
 gulp.task('watch', function () {
     webpack(require('./webpack.config')).watch({
@@ -26,4 +26,20 @@ gulp.task('watch', function () {
             colors: true
         }));
     });
+});
+
+gulp.task('lint', function () {
+    return gulp.src(['public/js/**/*.js'])
+        // eslint() attaches the lint output to the eslint property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failOnError last.
+        .pipe(eslint.failOnError());
+});
+
+gulp.task('default', ['lint', 'bundle:dev'], function () {
 });
