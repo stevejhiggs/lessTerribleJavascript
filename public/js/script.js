abde9408bda2ffbf417bb1a02bfd8e20d14352f3
Steve Higgs <shiggs@opentable.com>
2015-10-08 13:48:04 +0100

    generate a css bundle via web pack

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import data from './data';
 import {appendDataToList, sortByFirstName} from './listHandler';
+import styles from '../css/styles.scss';
 
 let listEntryPoint = $('#nameList tbody');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     resolve: {
-        extensions: ['', '.js']
+        extensions: ['', '.js', '.css', '.scss']
     },
     entry: [path.join(__dirname, '/public/js/script.js')],
     output: {
@@ -14,8 +15,21 @@ module.exports = {
             {test: /\.(js)$/, exclude: /node_modules/, loader: require.resolve('eslint-loader')}
         ],
         loaders: [
-            {test: /\.(js)$/, exclude: /node_modules/, loader: require.resolve('babel-loader')}
-        ]
+            {test: /\.(js)$/, exclude: /node_modules/, loader: require.resolve('babel-loader')},
+            // Extract css files
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract(require.resolve('style-loader'), require.resolve('css-loader'))
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract(require.resolve('style-loader'), require.resolve('css-loader') + '!' + require.resolve('sass-loader'))
+            }
+        ],
     },
-    plugins: []
+    plugins: [
+        new ExtractTextPlugin('bundle.css', {
+            allChunks: true
+        })
+    ]
 }
